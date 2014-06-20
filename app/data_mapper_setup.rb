8bfd3c28508ea05c6a351b30bfbc2120e2cbd91c
Margherita <m.serino@hotmail.it>
2014-06-20 15:19:05 +0100

    updated database_setup

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -1,7 +1,7 @@
 
 env = ENV["RACK_ENV"] || "development"
 
-DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/#{env}")
 
 # After declaring your models you should finalize them
 DataMapper.finalize
