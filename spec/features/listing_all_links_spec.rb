7600326f483246f02e04a56d2cad9258ccfab94d
mserino <m.serino@hotmail.it>
2014-08-16 16:01:25 +0200

    Listing all links - In the view page of the links, the description is shown

--- spec/features/listing_all_links_spec.rb
+++ spec/features/listing_all_links_spec.rb
@@ -5,15 +5,19 @@ feature "User browses the list of links" do
 	before(:each) do
 		Link.create(:url => "http://www.makersacademy.com",
 								:title => "Makers Academy",
+								:description => 'Learn to code in 12 weeks',
 								:tags => [Tag.first_or_create(:text => 'education')])
 		Link.create(:url => "http://www.google.com",
 								:title => "Google",
+								:description => 'Search engine',
 								:tags => [Tag.first_or_create(:text => 'search')])
 		Link.create(:url => "http://www.bing.com",
 								:title => "Bing",
+								:description => 'Lame search engine',
 								:tags => [Tag.first_or_create(:text => 'search')])
 		Link.create(:url => "http://www.code.org",
 								:title => "Code.org",
+								:description => 'Online coding school',
 								:tags => [Tag.first_or_create(:text => 'education')])
 	end
 
@@ -30,5 +34,13 @@ feature "User browses the list of links" do
 			expect(page).to have_content("Google")
 			expect(page).to have_content("Bing")
 		end
+	end
+
+	scenario 'and can see the description' do
+		visit '/'
+			expect(page).to have_content 'Learn to code in 12 weeks'
+			expect(page).to have_content 'Search engine'
+			expect(page).to have_content 'Lame search engine'
+			expect(page).to have_content 'Online coding school'
 	end
 end
